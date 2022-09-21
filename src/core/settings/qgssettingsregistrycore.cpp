d03a612588ecf4a4f46ab656126df8dcfed7240c
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-09-21 10:03:04 +0200

    migrate setting qgis/defaultCapabilitiesExpiry to registry (Integer, core)

--- src/core/settings/qgssettingsregistrycore.cpp
+++ src/core/settings/qgssettingsregistrycore.cpp
@@ -25,6 +25,7 @@
 #include "qgsnewsfeedparser.h"
 #include "qgsowsconnection.h"
 #include "qgsprocessing.h"
+#include "qgsgeonoderequest.h"
 #include "qgsvectortileconnection.h"
 #include "pal.h"
 #include "qgsrasterlayer.h"
@@ -99,6 +100,7 @@ QgsSettingsRegistryCore::QgsSettingsRegistryCore()
   addSettingsEntry( &settingsDigitizingReuseLastValues );
   addSettingsEntry( &settingsDigitizingDisableEnterAttributeValuesDialog );
   addSettingsEntry( &settingsDigitizingValidateGeometries );
+  addSettingsEntry( &settingsDefaultCapabilitiesExpiry );
   addSettingsEntry( &settingsZmax );
   addSettingsEntry( &settingsDigitizingOffsetJoinStyle );
   addSettingsEntry( &settingsZmin );
