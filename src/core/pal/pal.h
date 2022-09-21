d4cf150005c54fd3e68d7460def24d04b6c50004
Denis Rouzaud <denis.rouzaud@gmail.com>
2022-09-21 10:02:56 +0200

    migrate setting rendering/label_candidates_limit_points to class (Integer, core)

--- src/core/pal/pal.h
+++ src/core/pal/pal.h
@@ -35,6 +35,7 @@
 
 #include "qgis_core.h"
 #include "qgsgeometry.h"
+#include "qgssettingsentryimpl.h"
 #include "qgsgeos.h"
 #include "qgslabelingenginesettings.h"
 #include <QList>
@@ -84,6 +85,7 @@ namespace pal
 
     public:
 
+    static const inline QgsSettingsEntryInteger settingsRenderingLabelCandidatesLimitPoints = QgsSettingsEntryInteger( QStringLiteral( "label_candidates_limit_points" ), QgsSettings::Prefix::CORE_RENDERING, 0 ) SIP_SKIP;
       /**
        * \brief Create an new pal instance
        */
